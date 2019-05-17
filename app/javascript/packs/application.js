f1548d6b286ddf1b83bb330f851365582d747cc2
Frankie Ling <ingleson.fl@gmail.com>
2019-05-17 15:59:55 +0100

    Solve random problem that was not my fault

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -17,7 +17,3 @@
 
 console.log('Hello World from Webpacker')
 import 'bootstrap';
-
-
-<%= javascript_include_tag "application" %> <!-- from app/assets/javascripts/application.js -->
-<%= javascript_pack_tag "application" %>
